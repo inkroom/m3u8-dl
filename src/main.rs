e6a58f0946931cdc69bf6a0984abb5e6eb532ae1
inkbox <enpassPixiv@protonmail.com>
2024-12-18 17:11:44 +0800

    refactor: 更换log实现为env_logger

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,7 @@ use std::{ffi::CString, sync::Arc};
 use clap::Parser;
 use crypto::digest::Digest;
 use log::LevelFilter;
-use log4rs::append::console::ConsoleAppender;
-use log4rs::append::file::FileAppender;
-use log4rs::config::Logger;
+
 use m3u8_rs::Playlist;
 
 #[derive(Parser)]
@@ -304,62 +302,228 @@ fn aes128_cbc_decrypt(
             _ => continue,
         }
     }
-
     Ok(final_result)
 }
 
-impl Opt {
-    fn log(&self) {
-        let pattern = "{d(%Y-%m-%d %H:%M:%S)} : {m}{n}";
-
-        let mut s = log4rs::Config::builder()
-            .appender(
-                log4rs::config::Appender::builder().build(
-                    "stdout",
-                    Box::new(
-                        ConsoleAppender::builder()
-                            .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
-                                pattern,
-                            )))
-                            .build(),
-                    ),
-                ),
-            )
-            .logger(Logger::builder().build("rustls", LevelFilter::Off));
-
-        if let Some(log) = &self.log {
-            s = s.appender(
-                log4rs::config::Appender::builder().build(
-                    "file",
-                    Box::new(
-                        FileAppender::builder()
-                            .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
-                                pattern,
-                            )))
-                            .build(log)
-                            .unwrap(),
-                    ),
-                ),
-            );
-        }
-        log4rs::init_config(
-            s.build(
-                log4rs::config::Root::builder()
-                    .appender("stdout")
-                    .appender("file")
-                    .build(if self.verbose {
-                        LevelFilter::Debug
-                    } else {
-                        LevelFilter::Info
-                    }),
-            )
-            .unwrap(),
-        )
-        .unwrap();
+impl std::io::Write for Opt {
+    fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+        todo!()
     }
 
+    fn flush(&mut self) -> std::io::Result<()> {
+        todo!()
+    }
+}
+mod custom_log {
+    use crate::Opt;
+    use env_logger::fmt::Formatter;
+    use std::io::Write;
+    /// 时间戳转换，从1970年开始
+    pub(crate) fn time_display(value: u64) -> String {
+        do_time_display(value, 1970)
+    }
+
+    /// 时间戳转换，支持从不同年份开始计算
+    pub(crate) fn do_time_display(value: u64, start_year: u64) -> String {
+        // 先粗略定位到哪一年
+        // 以 365 来计算，年通常只会相比正确值更晚，剩下的秒数也就更多，并且有可能出现需要往前一年的情况
+
+        let per_year_sec = 365 * 24 * 60 * 60; // 平年的秒数
+
+        let mut year = value / per_year_sec;
+        // 剩下的秒数，如果这些秒数 不够填补闰年，比如粗略计算是 2024年，还有 86300秒，不足一天，那么中间有很多闰年，所以 年应该-1，只有-1，因为-2甚至更多 需要 last_sec > 365 * 86400，然而这是不可能的
+        let last_sec = value - (year) * per_year_sec;
+        year += start_year;
+
+        let mut leap_year_sec = 0;
+        // 计算中间有多少闰年，当前年是否是闰年不影响回退，只会影响后续具体月份计算
+        for y in start_year..year {
+            if is_leap(y) {
+                // 出现了闰年
+                leap_year_sec += 86400;
+            }
+        }
+        if last_sec < leap_year_sec {
+            // 不够填补闰年，年份应该-1
+            year -= 1;
+            // 上一年是闰年，所以需要补一天
+            if is_leap(year) {
+                leap_year_sec -= 86400;
+            }
+        }
+        // 剩下的秒数
+        let mut time = value - leap_year_sec - (year - start_year) * per_year_sec;
+
+        // 平年的月份天数累加
+        let mut day_of_year: [u64; 12] = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+        // 找到了 计算日期
+        let sec = time % 60;
+        time /= 60;
+        let min = time % 60;
+        time /= 60;
+        let hour = time % 24;
+        time /= 24;
+
+        // 计算是哪天，因为每个月不一样多，所以需要修改
+        if is_leap(year) {
+            day_of_year[1] += 1;
+        }
+        let mut month = 0;
+        for (index, ele) in day_of_year.iter().enumerate() {
+            if &time < ele {
+                month = index + 1;
+                time += 1; // 日期必须加一，否则 每年的 第 1 秒就成了第0天了
+                break;
+            }
+            time -= ele;
+        }
+
+        return format!(
+            "{:04}-{:02}-{:02} {:02}:{:02}:{:02}",
+            year,
+            month,
+            time,
+            hour + 8,
+            min,
+            sec
+        );
+    }
+    //
+    // 判断是否是闰年
+    //
+    fn is_leap(year: u64) -> bool {
+        return year % 4 == 0 && ((year % 100) != 0 || year % 400 == 0);
+    }
+    ///
+    /// 输出当前时间格式化
+    ///
+    /// 例如：
+    /// 2023-09-28T09:32:24Z
+    ///
+    pub(crate) fn time_format() -> String {
+        // 获取当前时间戳
+        let time = std::time::SystemTime::now()
+            .duration_since(std::time::UNIX_EPOCH)
+            .map(|v| v.as_secs())
+            .unwrap_or(0);
+
+        time_display(time)
+    }
+    struct Writer {
+        console: std::io::Stdout,
+        fs: Option<std::fs::File>,
+    }
+    impl Writer {
+        pub fn new(opt: &Opt) -> Self {
+            Writer {
+                console: std::io::stdout(),
+                fs: if let Some(f) = &opt.log {
+                    Some(
+                        std::fs::OpenOptions::new()
+                            .create(true)
+                            .append(true)
+                            .open(f.as_str())
+                            .unwrap(),
+                    )
+                } else {
+                    None
+                },
+            }
+        }
+    }
+    impl Write for Writer {
+        fn write(&mut self, buf: &[u8]) -> std::io::Result<usize> {
+            if let Some(fs) = &mut self.fs {
+                self.console.write(buf)?;
+                fs.write(buf)
+            } else {
+                self.console.write(buf)
+            }
+        }
+
+        fn flush(&mut self) -> std::io::Result<()> {
+            if let Some(fs) = &mut self.fs {
+                self.console.flush()?;
+                fs.flush()
+            } else {
+                self.console.flush()
+            }
+        }
+    }
+    pub(crate) fn init(opt: &Opt) -> Result<(), String> {
+        if opt.verbose {
+            std::env::set_var("RUST_LOG", "debug");
+        } else {
+            std::env::set_var("RUST_LOG", "info");
+        }
+
+        let mut s = env_logger::builder();
+        s.default_format()
+            .parse_default_env()
+            .format(|buf, record| writeln!(buf, "{}-{:?}: {}", time_format(),record.module_path(), record.args()))
+            .target(env_logger::Target::Pipe(Box::new(Writer::new(opt))));
+        if opt.verbose {
+            s.filter(Some("rustls"), log::LevelFilter::Off);
+        }
+        s.init();
+        Ok(())
+    }
+}
+
+impl Opt {
+    // fn log(&self) {
+    //     let pattern = "{d(%Y-%m-%d %H:%M:%S)} : {m}{n}";
+
+    //     let mut s = log4rs::Config::builder()
+    //         .appender(
+    //             log4rs::config::Appender::builder().build(
+    //                 "stdout",
+    //                 Box::new(
+    //                     log4rs::append::console::ConsoleAppender::builder()
+    //                         .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
+    //                             pattern,
+    //                         )))
+    //                         .build(),
+    //                 ),
+    //             ),
+    //         )
+    //         .logger(Logger::builder().build("rustls", LevelFilter::Off));
+
+    //     if let Some(log) = &self.log {
+    //         s = s.appender(
+    //             log4rs::config::Appender::builder().build(
+    //                 "file",
+    //                 Box::new(
+    //                     log4rs::append::file::FileAppender::builder()
+    //                         .encoder(Box::new(log4rs::encode::pattern::PatternEncoder::new(
+    //                             pattern,
+    //                         )))
+    //                         .build(log)
+    //                         .unwrap(),
+    //                 ),
+    //             ),
+    //         );
+    //     }
+    //     log4rs::init_config(
+    //         s.build(
+    //             log4rs::config::Root::builder()
+    //                 .appender("stdout")
+    //                 .appender("file")
+    //                 .build(if self.verbose {
+    //                     LevelFilter::Debug
+    //                 } else {
+    //                     LevelFilter::Info
+    //                 }),
+    //         )
+    //         .unwrap(),
+    //     )
+    //     .unwrap();
+    // }
+
     fn init(&mut self) -> Result<(), String> {
-        self.log();
+        // self.log();
+        custom_log::init(self)?;
 
         #[cfg(feature = "ureq")]
         {
